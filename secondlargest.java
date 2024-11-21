560fc83dc8e16c89fa5244f31446abe6305d7618
Niraj Kr Safi <91873441+Niraj-Kumar-Safi@users.noreply.github.com>
2024-11-21 13:39:54 +0530

    Update secondlargest.java

--- secondlargest.java
+++ secondlargest.java
@@ -1,15 +1,17 @@
 
-import java.util.Arrays;
+//Write a program to find the second largest number in a given integer array.
+
+import java.util.*;
 
 public class secondlargest {
     public static void main(String[] args) {
         int c=0;
         int[] array = {12, 35, 1, 10, 34, 1};
 
-        Arrays.sort(array);
-        c=array.length;
+        Arrays.sort(array); //sorting of elements in array
+        c=array.length; // finding the length of array
 
-        System.out.println("Second largest number :" +array[c-2]);
+        System.out.println("Second largest number :" +array[c-2]); //displaying second largest number in array 
     }
 }
         
